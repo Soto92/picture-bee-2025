4985b4f92abe7b65a6cab51588a774f10782dadd
Mauricio <mauricioasoto@gmail.com>
2025-09-03 16:18:34 -0300

    fix array

--- script.js
+++ script.js
@@ -1,105 +1,105 @@
 const flashcards = [
   {
-    word: "Bird",
-    img: "assets/Bird.png",
+    "word": "Bird",
+    "img": "day1/Bird.png"
   },
   {
-    word: "Black",
-    img: "assets/Black.png",
+    "word": "Black",
+    "img": "day1/Black.png"
   },
   {
-    word: "Blue",
-    img: "assets/Blue.png",
+    "word": "Blue",
+    "img": "day1/Blue.png"
   },
   {
-    word: "Brown",
-    img: "assets/Brown.png",
+    "word": "Brown",
+    "img": "day1/Brown.png"
   },
   {
-    word: "Cat",
-    img: "assets/Cat.png",
+    "word": "Cat",
+    "img": "day1/Cat.png"
   },
   {
-    word: "Dog",
-    img: "assets/Dog.png",
+    "word": "Dog",
+    "img": "day1/Dog.png"
   },
   {
-    word: "Eight",
-    img: "assets/Eight.png",
+    "word": "Eight",
+    "img": "day1/Eight.png"
   },
   {
-    word: "Fish",
-    img: "assets/Fish.png",
+    "word": "Fish",
+    "img": "day1/Fish.png"
   },
   {
-    word: "Five",
-    img: "assets/Five.png",
+    "word": "Five",
+    "img": "day1/Five.png"
   },
   {
-    word: "Four",
-    img: "assets/Four.png",
+    "word": "Four",
+    "img": "day1/Four.png"
   },
   {
-    word: "Green",
-    img: "assets/Green.png",
+    "word": "Green",
+    "img": "day1/Green.png"
   },
   {
-    word: "Hen",
-    img: "assets/Hen.png",
+    "word": "Hen",
+    "img": "day1/Hen.png"
   },
   {
-    word: "Nine",
-    img: "assets/Nine.png",
+    "word": "Nine",
+    "img": "day1/Nine.png"
   },
   {
-    word: "One",
-    img: "assets/One.png",
+    "word": "One",
+    "img": "day1/One.png"
   },
   {
-    word: "Orange",
-    img: "assets/Orange.png",
+    "word": "Orange",
+    "img": "day1/Orange.png"
   },
   {
-    word: "Pink",
-    img: "assets/Pink.png",
+    "word": "Pink",
+    "img": "day1/Pink.png"
   },
   {
-    word: "Purple",
-    img: "assets/Purple.png",
+    "word": "Purple",
+    "img": "day1/Purple.png"
   },
   {
-    word: "Red",
-    img: "assets/Red.png",
+    "word": "Red",
+    "img": "day1/Red.png"
   },
   {
-    word: "Seven",
-    img: "assets/Seven.png",
+    "word": "Seven",
+    "img": "day1/Seven.png"
   },
   {
-    word: "Six",
-    img: "assets/Six.png",
+    "word": "Six",
+    "img": "day1/Six.png"
   },
   {
-    word: "Ten",
-    img: "assets/Ten.png",
+    "word": "Ten",
+    "img": "day1/Ten.png"
   },
   {
-    word: "Three",
-    img: "assets/Three.png",
+    "word": "Three",
+    "img": "day1/Three.png"
   },
   {
-    word: "Two",
-    img: "assets/Two.png",
+    "word": "Two",
+    "img": "day1/Two.png"
   },
   {
-    word: "White",
-    img: "assets/White.png",
+    "word": "White",
+    "img": "day1/White.png"
   },
   {
-    word: "Yellow",
-    img: "assets/Yellow.png",
-  },
-];
+    "word": "Yellow",
+    "img": "day1/Yellow.png"
+  }
+]
 
 let currentIndex = 0;
 
