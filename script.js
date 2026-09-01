f452046048f5d4315a5d67334231e6f056fd274f
Mauricio <mauricioasoto@gmail.com>
2026-09-01 12:20:09 -0300

    add all the pictures

---
+++ script.js
@@ -0,0 +1,245 @@
+const flashcards = {
+  "1": [
+    {
+      "word": "Apple",
+      "img": "day1/Apple.png"
+    },
+    {
+      "word": "Baby",
+      "img": "day1/Baby.png"
+    },
+    {
+      "word": "Banana",
+      "img": "day1/Banana.png"
+    },
+    {
+      "word": "Bike",
+      "img": "day1/Bike.png"
+    },
+    {
+      "word": "Blue",
+      "img": "day1/Blue.png"
+    },
+    {
+      "word": "Broccoly",
+      "img": "day1/Broccoly.png"
+    },
+    {
+      "word": "Bus",
+      "img": "day1/Bus.png"
+    },
+    {
+      "word": "Car",
+      "img": "day1/Car.png"
+    },
+    {
+      "word": "Cat",
+      "img": "day1/Cat.png"
+    },
+    {
+      "word": "Daddy",
+      "img": "day1/Daddy.png"
+    },
+    {
+      "word": "Dog",
+      "img": "day1/Dog.png"
+    },
+    {
+      "word": "Elephant",
+      "img": "day1/Elephant.png"
+    },
+    {
+      "word": "Happy",
+      "img": "day1/Happy.png"
+    },
+    {
+      "word": "Lion",
+      "img": "day1/Lion.png"
+    },
+    {
+      "word": "Mommy",
+      "img": "day1/Mommy.png"
+    },
+    {
+      "word": "Orange",
+      "img": "day1/Orange.png"
+    },
+    {
+      "word": "Pink",
+      "img": "day1/Pink.png"
+    },
+    {
+      "word": "Sunny",
+      "img": "day1/Sunny.png"
+    },
+    {
+      "word": "Train",
+      "img": "day1/Train.png"
+    },
+    {
+      "word": "Yellow",
+      "img": "day1/Yellow.png"
+    }
+  ],
+  "2": [
+    {
+      "word": "Cloudy",
+      "img": "day2/Cloudy.png"
+    },
+    {
+      "word": "Eight",
+      "img": "day2/Eight.png"
+    },
+    {
+      "word": "Five",
+      "img": "day2/Five.png"
+    },
+    {
+      "word": "Four",
+      "img": "day2/Four.png"
+    },
+    {
+      "word": "Green",
+      "img": "day2/Green.png"
+    },
+    {
+      "word": "Nine",
+      "img": "day2/Nine.png"
+    },
+    {
+      "word": "One",
+      "img": "day2/One.png"
+    },
+    {
+      "word": "Orange",
+      "img": "day2/Orange.png"
+    },
+    {
+      "word": "Purple",
+      "img": "day2/Purple.png"
+    },
+    {
+      "word": "Rainy",
+      "img": "day2/Rainy.png"
+    },
+    {
+      "word": "Red",
+      "img": "day2/Red.png"
+    },
+    {
+      "word": "Sad",
+      "img": "day2/Sad.png"
+    },
+    {
+      "word": "School",
+      "img": "day2/School.png"
+    },
+    {
+      "word": "Seven",
+      "img": "day2/Seven.png"
+    },
+    {
+      "word": "Six",
+      "img": "day2/Six.png"
+    },
+    {
+      "word": "Teacher",
+      "img": "day2/Teacher.png"
+    },
+    {
+      "word": "Ten",
+      "img": "day2/Ten.png"
+    },
+    {
+      "word": "Three",
+      "img": "day2/Three.png"
+    },
+    {
+      "word": "Two",
+      "img": "day2/Two.png"
+    }
+  ],
+  "3": [
+    {
+      "word": "Airplane",
+      "img": "day3/Airplane.png"
+    },
+    {
+      "word": "Angry",
+      "img": "day3/Angry.png"
+    },
+    {
+      "word": "Beet",
+      "img": "day3/Beet.png"
+    },
+    {
+      "word": "Boat",
+      "img": "day3/Boat.png"
+    },
+    {
+      "word": "Book",
+      "img": "day3/Book.png"
+    },
+    {
+      "word": "Carrot",
+      "img": "day3/Carrot.png"
+    },
+    {
+      "word": "Fish",
+      "img": "day3/Fish.png"
+    },
+    {
+      "word": "Giraffe",
+      "img": "day3/Giraffe.png"
+    },
+    {
+      "word": "Grapes",
+      "img": "day3/Grapes.png"
+    },
+    {
+      "word": "Hungry",
+      "img": "day3/Hungry.png"
+    },
+    {
+      "word": "Kangaroo",
+      "img": "day3/Kangaroo.png"
+    },
+    {
+      "word": "Monkey",
+      "img": "day3/Monkey.png"
+    },
+    {
+      "word": "Motorcycle",
+      "img": "day3/Motorcycle.png"
+    },
+    {
+      "word": "Pencil Case",
+      "img": "day3/Pencil Case.png"
+    },
+    {
+      "word": "Pencil",
+      "img": "day3/Pencil.png"
+    },
+    {
+      "word": "Pineapple",
+      "img": "day3/Pineapple.png"
+    },
+    {
+      "word": "Potato",
+      "img": "day3/Potato.png"
+    },
+    {
+      "word": "Shark",
+      "img": "day3/Shark.png"
+    },
+    {
+      "word": "Strawberry",
+      "img": "day3/Strawberry.png"
+    },
+    {
+      "word": "Watermelon",
+      "img": "day3/Watermelon.png"
+    }
+  ]
+}
+
