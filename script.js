8e3884be40c3e06692a54aeaa659effbaa7f89b8
Mauricio <mauricioasoto@gmail.com>
2026-09-01 12:47:31 -0300

    fix styles

--- script.js
+++ script.js
@@ -1,245 +1,301 @@
 const flashcards = {
-  "1": [
+  1: [
     {
-      "word": "Apple",
-      "img": "day1/Apple.png"
+      word: "Apple",
+      img: "day1/Apple.png",
     },
     {
-      "word": "Baby",
-      "img": "day1/Baby.png"
+      word: "Baby",
+      img: "day1/Baby.png",
     },
     {
-      "word": "Banana",
-      "img": "day1/Banana.png"
+      word: "Banana",
+      img: "day1/Banana.png",
     },
     {
-      "word": "Bike",
-      "img": "day1/Bike.png"
+      word: "Bike",
+      img: "day1/Bike.png",
     },
     {
-      "word": "Blue",
-      "img": "day1/Blue.png"
+      word: "Blue",
+      img: "day1/Blue.png",
     },
     {
-      "word": "Broccoly",
-      "img": "day1/Broccoly.png"
+      word: "Broccoly",
+      img: "day1/Broccoly.png",
     },
     {
-      "word": "Bus",
-      "img": "day1/Bus.png"
+      word: "Bus",
+      img: "day1/Bus.png",
     },
     {
-      "word": "Car",
-      "img": "day1/Car.png"
+      word: "Car",
+      img: "day1/Car.png",
     },
     {
-      "word": "Cat",
-      "img": "day1/Cat.png"
+      word: "Cat",
+      img: "day1/Cat.png",
     },
     {
-      "word": "Daddy",
-      "img": "day1/Daddy.png"
+      word: "Daddy",
+      img: "day1/Daddy.png",
     },
     {
-      "word": "Dog",
-      "img": "day1/Dog.png"
+      word: "Dog",
+      img: "day1/Dog.png",
     },
     {
-      "word": "Elephant",
-      "img": "day1/Elephant.png"
+      word: "Elephant",
+      img: "day1/Elephant.png",
     },
     {
-      "word": "Happy",
-      "img": "day1/Happy.png"
+      word: "Happy",
+      img: "day1/Happy.png",
     },
     {
-      "word": "Lion",
-      "img": "day1/Lion.png"
+      word: "Lion",
+      img: "day1/Lion.png",
     },
     {
-      "word": "Mommy",
-      "img": "day1/Mommy.png"
+      word: "Mommy",
+      img: "day1/Mommy.png",
     },
     {
-      "word": "Orange",
-      "img": "day1/Orange.png"
+      word: "Orange",
+      img: "day1/Orange.png",
     },
     {
-      "word": "Pink",
-      "img": "day1/Pink.png"
+      word: "Pink",
+      img: "day1/Pink.png",
     },
     {
-      "word": "Sunny",
-      "img": "day1/Sunny.png"
+      word: "Sunny",
+      img: "day1/Sunny.png",
     },
     {
-      "word": "Train",
-      "img": "day1/Train.png"
+      word: "Train",
+      img: "day1/Train.png",
     },
     {
-      "word": "Yellow",
-      "img": "day1/Yellow.png"
-    }
+      word: "Yellow",
+      img: "day1/Yellow.png",
+    },
   ],
-  "2": [
+  2: [
     {
-      "word": "Cloudy",
-      "img": "day2/Cloudy.png"
+      word: "Cloudy",
+      img: "day2/Cloudy.png",
     },
     {
-      "word": "Eight",
-      "img": "day2/Eight.png"
+      word: "Eight",
+      img: "day2/Eight.png",
     },
     {
-      "word": "Five",
-      "img": "day2/Five.png"
+      word: "Five",
+      img: "day2/Five.png",
     },
     {
-      "word": "Four",
-      "img": "day2/Four.png"
+      word: "Four",
+      img: "day2/Four.png",
     },
     {
-      "word": "Green",
-      "img": "day2/Green.png"
+      word: "Green",
+      img: "day2/Green.png",
     },
     {
-      "word": "Nine",
-      "img": "day2/Nine.png"
+      word: "Nine",
+      img: "day2/Nine.png",
     },
     {
-      "word": "One",
-      "img": "day2/One.png"
+      word: "One",
+      img: "day2/One.png",
     },
     {
-      "word": "Orange",
-      "img": "day2/Orange.png"
+      word: "Orange",
+      img: "day2/Orange.png",
     },
     {
-      "word": "Purple",
-      "img": "day2/Purple.png"
+      word: "Purple",
+      img: "day2/Purple.png",
     },
     {
-      "word": "Rainy",
-      "img": "day2/Rainy.png"
+      word: "Rainy",
+      img: "day2/Rainy.png",
     },
     {
-      "word": "Red",
-      "img": "day2/Red.png"
+      word: "Red",
+      img: "day2/Red.png",
     },
     {
-      "word": "Sad",
-      "img": "day2/Sad.png"
+      word: "Sad",
+      img: "day2/Sad.png",
     },
     {
-      "word": "School",
-      "img": "day2/School.png"
+      word: "School",
+      img: "day2/School.png",
     },
     {
-      "word": "Seven",
-      "img": "day2/Seven.png"
+      word: "Seven",
+      img: "day2/Seven.png",
     },
     {
-      "word": "Six",
-      "img": "day2/Six.png"
+      word: "Six",
+      img: "day2/Six.png",
     },
     {
-      "word": "Teacher",
-      "img": "day2/Teacher.png"
+      word: "Teacher",
+      img: "day2/Teacher.png",
     },
     {
-      "word": "Ten",
-      "img": "day2/Ten.png"
+      word: "Ten",
+      img: "day2/Ten.png",
     },
     {
-      "word": "Three",
-      "img": "day2/Three.png"
+      word: "Three",
+      img: "day2/Three.png",
     },
     {
-      "word": "Two",
-      "img": "day2/Two.png"
-    }
+      word: "Two",
+      img: "day2/Two.png",
+    },
   ],
-  "3": [
+  3: [
     {
-      "word": "Airplane",
-      "img": "day3/Airplane.png"
+      word: "Airplane",
+      img: "day3/Airplane.png",
     },
     {
-      "word": "Angry",
-      "img": "day3/Angry.png"
+      word: "Angry",
+      img: "day3/Angry.png",
     },
     {
-      "word": "Beet",
-      "img": "day3/Beet.png"
+      word: "Beet",
+      img: "day3/Beet.png",
     },
     {
-      "word": "Boat",
-      "img": "day3/Boat.png"
+      word: "Boat",
+      img: "day3/Boat.png",
     },
     {
-      "word": "Book",
-      "img": "day3/Book.png"
+      word: "Book",
+      img: "day3/Book.png",
     },
     {
-      "word": "Carrot",
-      "img": "day3/Carrot.png"
+      word: "Carrot",
+      img: "day3/Carrot.png",
     },
     {
-      "word": "Fish",
-      "img": "day3/Fish.png"
+      word: "Fish",
+      img: "day3/Fish.png",
     },
     {
-      "word": "Giraffe",
-      "img": "day3/Giraffe.png"
+      word: "Giraffe",
+      img: "day3/Giraffe.png",
     },
     {
-      "word": "Grapes",
-      "img": "day3/Grapes.png"
+      word: "Grapes",
+      img: "day3/Grapes.png",
     },
     {
-      "word": "Hungry",
-      "img": "day3/Hungry.png"
+      word: "Hungry",
+      img: "day3/Hungry.png",
     },
     {
-      "word": "Kangaroo",
-      "img": "day3/Kangaroo.png"
+      word: "Kangaroo",
+      img: "day3/Kangaroo.png",
     },
     {
-      "word": "Monkey",
-      "img": "day3/Monkey.png"
+      word: "Monkey",
+      img: "day3/Monkey.png",
     },
     {
-      "word": "Motorcycle",
-      "img": "day3/Motorcycle.png"
+      word: "Motorcycle",
+      img: "day3/Motorcycle.png",
     },
     {
-      "word": "Pencil Case",
-      "img": "day3/Pencil Case.png"
+      word: "Pencil Case",
+      img: "day3/Pencil Case.png",
     },
     {
-      "word": "Pencil",
-      "img": "day3/Pencil.png"
+      word: "Pencil",
+      img: "day3/Pencil.png",
     },
     {
-      "word": "Pineapple",
-      "img": "day3/Pineapple.png"
+      word: "Pineapple",
+      img: "day3/Pineapple.png",
     },
     {
-      "word": "Potato",
-      "img": "day3/Potato.png"
+      word: "Potato",
+      img: "day3/Potato.png",
     },
     {
-      "word": "Shark",
-      "img": "day3/Shark.png"
+      word: "Shark",
+      img: "day3/Shark.png",
     },
     {
-      "word": "Strawberry",
-      "img": "day3/Strawberry.png"
+      word: "Strawberry",
+      img: "day3/Strawberry.png",
     },
     {
-      "word": "Watermelon",
-      "img": "day3/Watermelon.png"
-    }
-  ]
+      word: "Watermelon",
+      img: "day3/Watermelon.png",
+    },
+  ],
+};
+
+let currentSet = [];
+let currentIndex = 0;
+
+const menu = document.getElementById("menu");
+const game = document.getElementById("game");
+const card = document.getElementById("flashcard");
+const inner = card.querySelector(".card-inner");
+const cardImage = document.getElementById("card-image");
+const cardWord = document.getElementById("card-word");
+const nextBtn = document.getElementById("nextBtn");
+const homeBtn = document.getElementById("homeBtn");
+
+function setCard(i) {
+  cardImage.src = currentSet[i].img;
+  cardWord.textContent = currentSet[i].word;
 }
 
+function startDay(day) {
+  currentSet = flashcards[day];
+  currentIndex = 0;
+  setCard(currentIndex);
+  menu.classList.add("hidden");
+  game.classList.remove("hidden");
+}
+
+document.querySelectorAll(".day-btn").forEach((btn) => {
+  btn.addEventListener("click", () => startDay(btn.dataset.day));
+});
+
+card.addEventListener("click", () => {
+  card.classList.toggle("flipped");
+});
+
+nextBtn.addEventListener("click", () => {
+  const proceed = () => {
+    currentIndex = (currentIndex + 1) % currentSet.length;
+    setCard(currentIndex);
+  };
+  if (card.classList.contains("flipped")) {
+    const onEnd = (e) => {
+      if (e.propertyName !== "transform") return;
+      inner.removeEventListener("transitionend", onEnd);
+      proceed();
+    };
+    inner.addEventListener("transitionend", onEnd);
+    card.classList.remove("flipped");
+  } else {
+    proceed();
+  }
+});
+
+homeBtn.addEventListener("click", () => {
+  game.classList.add("hidden");
+  menu.classList.remove("hidden");
+  card.classList.remove("flipped");
+});
